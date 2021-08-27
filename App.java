b468fdc0ab2075e3e8ed3bad866476c91629a051
Chen Yun <chenyun964@gmail.com>
2021-08-27 10:42:30 +0800

    first version

--- App.java
+++ App.java
@@ -3,7 +3,27 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Eneter your region:");
-        String str = sc.nextLine();
+
+        System.out.print("FROM: ");
+        String from = sc.nextLine();
+
+        System.out.print("TO: ");
+        String to = sc.nextLine();
+
+        double rate = checkRate(from, to);
+        System.out.print("Please enter the weight: ");
+        double weight = sc.nextDouble();
+
+        System.out.println("Price: $" + rate * weight);
+
+    }
+
+    public static double checkRate(String from, String to){
+        if(from.equals("AP")  && to.equals("EU")){
+            return 8084.00;
+        } else{
+            return 1000.00;
+        }
+        
     }
 }
